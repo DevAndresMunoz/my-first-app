33193aa34af730ea69f7c17a747a242ab50d206a
DevAndresMunoz <andresgabrielmunoz@gmail.com>
2024-11-04 22:34:30 -0700

    basic layout for component

---
+++ src/components/Post/post.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './Post.css';
+
+class Post extends Component {
+  render() {
+    return (
+      <div className="post">
+        <div className="header-container">
+            <div className="userInfo">
+              <img src="https://placehold.co/100x100" alt="user-image"></img>
+              <div>
+                  <h3>user-handle</h3>
+                  <h4>user-location</h4>
+              </div>
+            </div>
+            <i class="fa-solid fa-ellipsis"></i>
+        </div>
+        <div className="image-container">
+          <img src='https://images.unsplash.com/photo-1552053831-71594a27632d?q=80&w=1924&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='dog'></img>
+          <h2>Overlay Text</h2>
+        </div>
+        <div className="footer">
+          <i class="fa-regular fa-heart"></i>
+          <i class="fa-regular fa-comment"></i>
+          <i class="fa-regular fa-paper-plane"></i>
+          <i class="fa-regular fa-bookmark"></i>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Post;
